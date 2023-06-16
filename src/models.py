91aa7d8ab5ed3407a91be135b7e3dc889a4742bf
Mirkopantufla <mikish_ash@hotmail.com>
2023-06-15 21:43:21 -0400

    database de starwars blog con 4 tablas terminada

--- src/models.py
+++ src/models.py
@@ -1,32 +1,58 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Date, UniqueConstraint
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    username = Column(String(100))
+    first_name = Column(String(100))
+    last_name = Column(String(100))
 
-    def to_dict(self):
-        return {}
+class Favorite(Base):
+    __tablename__ = 'favorites'
+    id = Column(Integer, primary_key=True)
+    people_id = Column(Integer, ForeignKey('peoples.id'))
+    planet_id = Column(Integer, ForeignKey('planets.id'))
+    user_id = Column(Integer, ForeignKey('users.id'))
+
+class People(Base):
+    __tablename__ = 'peoples'
+    id = Column(Integer, primary_key=True)
+    height = Column(String(20))
+    mass = Column(String(20))
+    hair_color = Column(String(50))
+    skin_color = Column(String(50))
+    eye_color = Column(String(50))
+    birth_year = Column(Date)
+    gender = Column(String(10))
+    name = Column(String(100))
+    planet_id = Column(String(100), ForeignKey('planets.id'))
+
+class Planet(Base):
+    __tablename__ = 'planets'
+    id = Column(Integer, primary_key=True)
+    diameter = Column(Integer)
+    rotation_period = Column(Integer)
+    orbital_period = Column(Integer)
+    gravity = Column(String)
+    population = Column(Integer)
+    climate = Column(String)
+    terrain = Column(String)
+    surface_water = Column(Integer)
+    name = Column(String)
+    url = Column(String)
 
 ## Draw from SQLAlchemy base
-render_er(Base, 'diagram.png')
+try:
+    result = render_er(Base, 'diagram.png')
+    print("Success! Check the diagram.png file")
+except Exception as e:
+    print("There was a problem genering the diagram")
+    raise e
